9b9872079b7ddaf34e54f894bf92c82277d7270a
Cristiano Ceccarelli <crilose@hotmail.it>
2022-01-27 12:40:09 +0100

    Started Main Activity

--- app/src/main/java/com/example/seatandorder/Activity/IntroActivity.java
+++ app/src/main/java/com/example/seatandorder/Activity/IntroActivity.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import com.example.seatandorder.MainActivity;
 import com.example.seatandorder.R;
 
 public class IntroActivity extends AppCompatActivity {
--- app/src/main/java/com/example/seatandorder/Activity/MainActivity.java
+++ app/src/main/java/com/example/seatandorder/Activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.seatandorder;
+package com.example.seatandorder.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.seatandorder.R;
 
 public class MainActivity extends AppCompatActivity {
 
